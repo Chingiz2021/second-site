3ba337f7131d3257a9995dcfe43e843aef50c25a
gleb89 <glebhleb89@icloud.com>
2021-11-22 16:11:49 +0300

    ffff

--- static/js/index.js
+++ static/js/index.js
@@ -253,10 +253,10 @@ function activefunc(element) {
 }
 if(document.documentElement.clientWidth < 990){
   if(clickbutton){
-    clickbutton.style = 'margin: 10px;padding: 0;border: 2px #EB5534 solid;width: 20px;height: 20px;'
+    clickbutton.style = 'margin: 10px;padding: 0;border: 3px #EB5534 solid;width: 20px;height: 20px;'
   }
   if (clickbutton != element){
-    element.style = 'border: 2px #a9a7a7 solid;margin: 10px;padding: 0;width: 20px;height: 20px;'
+    element.style = 'border: 3px #a9a7a7 solid;margin: 10px;padding: 0;width: 20px;height: 20px;'
     clickbutton = element
   }
 }
