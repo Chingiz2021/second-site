2e77c12a6f7a33de6460418a2009e14ddad1c99a
gleb89 <glebhleb89@icloud.com>
2023-01-30 12:16:33 +0300

    test

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the URL to check
-url="https://api.telegram.org/bot6105342986:AAGvBVv9fnemOxPhMarvIbSZSI--0nYeU1w/setWebHook?url=https://unwanted.ae/api/bot"
+url="https://api.telegram.org/bot5959383988:AAH3Nadk4iRp56DNfOPb7uxJMQ6WZhXOIus/setWebHook?url=https://unwanted.ae/api/bot"
 
 # Send a request to the URL and store the response code
 response_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
