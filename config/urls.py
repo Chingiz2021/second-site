671d234d2f6fcb8be8ef28de5b86709d03d2fd3c
gleb89 <glebhleb89@icloud.com>
2023-01-30 12:12:21 +0300

    test

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('subscriptions.urls')),
-    path('bot/', include('bot.urls')),
+    path('api/bot/', include('bot.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
